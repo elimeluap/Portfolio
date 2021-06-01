e0a271bded85fe188055ee7635898aa2f2824d57
Paul-Emile Antoine <antoinepe.contact@gmail.com>
2021-06-01 22:38:31 +0200

    Création des méthodes register, login et logout du AuthController

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RealisationsController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Routes publiques
 Route::resource('/realisations', RealisationsController::class, ['except' => ['show', 'create', 'edit']]);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Routes protégées
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});

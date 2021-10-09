0e920f215f818f97008d68a40c371122278b4f12
Paul <antoinepe.contact@gmail.com>
2021-10-09 22:18:25 +0200

    Mise à jour du template du composant Footer

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,7 +1,50 @@
 <template>
   <!-- Footer -->
-  <footer class="py-4">
-    <div class="text-center">&copy; Paul-Emile ANTOINE 2021</div>
+  <footer>
+    <div class="container">
+      <div
+        class="
+          d-flex
+          flex-column flex-md-row
+          justify-content-md-end
+          align-items-center
+        "
+      >
+        <div class="d-flex justify-content-end align-items-center">
+          <div class="mr-md-4 social-footer">
+            <ul
+              class="
+                d-flex
+                justify-content-center
+                align-items-center
+                mb-1 mb-md-0
+              "
+            >
+              <li class="mr-4">
+                <a href="https://codepen.io/peantoine-dev" target="_blank"
+                  ><i class="fab fa-codepen"></i
+                ></a>
+              </li>
+              <li class="mr-4">
+                <a href="https://github.com/peantoine-dev" target="_blank"
+                  ><i class="fab fa-github"></i
+                ></a>
+              </li>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/paul-emile-antoine-8628bb128/"
+                  target="_blank"
+                  ><i class="fab fa-linkedin"></i
+                ></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="text-white text-center text-md-left">
+          &copy; Paul-Emile ANTOINE 2021
+        </div>
+      </div>
+    </div>
   </footer>
 </template>
 

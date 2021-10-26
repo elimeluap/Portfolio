901a8f88256b02ee96e2a226557a5b1a483156dd
Paul <antoinepe.contact@gmail.com>
2021-10-26 18:39:23 +0200

    Ajout des balises meta

--- resources/views/template/partials/_head.blade.php
+++ resources/views/template/partials/_head.blade.php
@@ -1,6 +1,12 @@
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+<meta name="title" content="Paul-Emile ANTOINE | Développeur web">
+<meta name="description" content="Paul-Emile ANTOINE, développeur web front-end. Vous avez une idée de site web en tête mais vous ne savez pas comment vous y prendre, contactez-moi !">
+<meta name="keywords" content="creation, sites, web, developpeur">
+<meta name="robots" content="index, follow">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="language" content="French">
 <title>Paul-Emile ANTOINE | Développeur web</title>
 <link rel="icon" href="{{ asset('assets/images/0.jpg') }}" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
